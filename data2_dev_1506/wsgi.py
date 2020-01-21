3e21586e0c032263c03cb6ce81ef5701820bc839
Crowdbotics <team@crowdbotics.com>
2020-01-21 11:17:05 +0000

    Update models

--- data2_dev_1506/wsgi.py
+++ data2_dev_1506/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data2_dev_1506.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "data2_dev_1506.settings")
 
 application = get_wsgi_application()
